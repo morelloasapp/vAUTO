5d5be2291675afd8434c00149cee49be930f6f8d
morelloasapp <eduarddraghici0@gmail.com>
2025-12-03 14:14:38 +0200

    Remove PromoBanner from Home component
    Removed PromoBanner component and related promo logic.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import Hero from "@/components/Hero";
 import VehicleSelector from "@/components/VehicleSelector";
-import PromoBanner from "@/components/PromoBanner";
 import Categories from "@/components/Categories";
 import DetailedCategories from "@/components/DetailedCategories";
 import ProductGrid from "@/components/ProductGrid";
@@ -11,34 +10,12 @@ import BrandLogos from "@/components/BrandLogos";
 import Newsletter from "@/components/Newsletter";
 
 export default function Home() {
-  const promoEndDate = new Date();
-  promoEndDate.setDate(promoEndDate.getDate() + 5);
-
   return (
     <div>
       <Hero />
       <VehicleSelector />
-      
-      <section className="py-8 bg-background">
-        <div className="max-w-7xl mx-auto px-4">
-          <div className="grid md:grid-cols-2 gap-6">
-            <PromoBanner
-              title="Ofertă Specială Frânare"
-              discount="-35%"
-              endDate={promoEndDate}
-              image=""
-              variant="urgent"
-            />
-            <PromoBanner
-              title="Reduceri Motor & Suspensie"
-              discount="-25%"
-              endDate={new Date(promoEndDate.getTime() + 2 * 24 * 60 * 60 * 1000)}
-              image=""
-              variant="default"
-            />
-          </div>
-        </div>
-      </section>
+
+      {/* PromoBanner a fost eliminat */}
 
       <Categories />
       <DetailedCategories />
@@ -51,3 +28,4 @@ export default function Home() {
     </div>
   );
 }
+
